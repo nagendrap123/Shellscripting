44c1e752ca6b4c4f38dd29617ee18dfc6b08358f
nagendrap123 <nagendrap0820@gmail.com>
2024-04-18 16:23:43 +0530

    mmm

--- command_sub.sh
+++ command_sub.sh
@@ -3,7 +3,7 @@ echo "your are using the $0"
 current_path=`pwd`
 current_ip=`hostname -I`
 kernel_name=`uname -n`
-echo "current directory $(current_path)
-echo "hostname of the Os $(current_ip)
-echo "kernel_name $(kernel_name)
+echo "current directory $(current_path)"
+echo "hostname of the Os $(current_ip)"
+echo "kernel_name $(kernel_name)"
 
