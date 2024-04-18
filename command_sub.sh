05b94d580efe602abbdb91908da65bfb648a1cf1
nagendrap123 <nagendrap0820@gmail.com>
2024-04-18 16:21:07 +0530

    mmm

--- command_sub.sh
+++ command_sub.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "your are using the $0"
-cuurent_path=`pwd`
-current_ip='hostname -I`
+current_path=`pwd`
+current_ip=`hostname -I`
 kernel_name=`uname -n`
+echo "current directory $(current_path)
+echo "hostname of the Os $(current_ip)
+echo "kernel_name $(kernel_name)
+
