eb0c1cbcc1b68ba02713c24055a0e113db942d03
nagendrap123 <nagendrap0820@gmail.com>
2024-04-18 22:00:44 +0530

    mmm

---
+++ arrays_variable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Define an array of server addresses
+servers=("server1.example.com" "server2.example.com" "server3.example.com")
+echo "${servers[0]}"
+echo "${servers[1]}"
+echo "${servers[2]}"
+echo "${servers[@]}"
+
+# Define an array of application names
+applications=("app1" "app2" "app3")
+
+echo "${applications[0]}"
+echo "${applications[1]}"
+echo "${applications[2]}"
+echo "${applications[@]}"
