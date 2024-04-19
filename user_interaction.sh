510120b47357cde625abdd74aa06c0e1b31237ae
nagendrap123 <nagendrap0820@gmail.com>
2024-04-19 09:50:02 +0530

    user interaction

---
+++ user_interaction.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script demonstrates the usage of the read command
+# to collect user input and perform actions based on it.
+
+# Prompt the user to enter their name
+echo "Welcome to the DevOps tool!"
+read -p "enter the username: " username
+echo "user name is $username"
+
