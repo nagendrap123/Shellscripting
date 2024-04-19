194cb0eba9eb27f0007793e0fabb435120d05240
nagendrap123 <nagendrap0820@gmail.com>
2024-04-19 10:29:48 +0530

    ram

--- ram_utilisation.sh
+++ ram_utilisation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script uses the 'free -m' command to display RAM utilization
-# and performs arithmetic operations to calculate used and free RAM.
+# and calculates the percentage of RAM used.
 
 # Execute 'free -m' command and store the output in a variable
 ram_info=$(free -m)
@@ -14,8 +14,11 @@ free_ram=$(echo "$ram_info" | awk 'NR==2 {print $4}')
 # Calculate used RAM by subtracting free RAM from total RAM
 used_ram=$((total_ram - free_ram))
 
+# Calculate percentage of RAM used
+ram_utilization=$((used_ram * 100 / total_ram))
+
 # Display RAM utilization
 echo "Total RAM: $total_ram MB"
 echo "Used RAM: $used_ram MB"
 echo "Free RAM: $free_ram MB"
-
+echo "RAM Utilization: $ram_utilization%"
