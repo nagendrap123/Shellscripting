5948f2fe6ddf94dc771627b70fb0f490f61d146e
nagendrap123 <nagendrap0820@gmail.com>
2024-04-20 00:59:17 +0530

    logical operator

--- logical_ops(OR).sh
+++ logical_ops(OR).sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-#AND operator
+#OR operator
 read -p "enter your age:" age
 read -p "enter your country:" country
-if [ $age -ge 18 ] && [ $country == "india" ]
+if [ $age -ge 18 ] || [ $country == "india" ]
 then 
 echo "you can vote"
 else 
 echo "tou cant vote"
 fi
-
