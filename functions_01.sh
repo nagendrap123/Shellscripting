11b02276438e72367b2467b6930701fa73bb2d01
nagendrap123 <nagendrap0820@gmail.com>
2024-04-21 00:48:14 +0530

    functions

---
+++ functions_01.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#how to create the function
+function welcomeNote {
+    echo "------------------"
+    echo "welcome"
+    echo "-----------------"
+
+}
+#to call the function 
+welcomeNote
+welcomeNote
+welcomeNote
