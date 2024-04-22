d033c41927cbdf8dfdd47fd62040bb85066cd33b
nagendrap123 <nagendrap0820@gmail.com>
2024-04-22 08:19:28 +0530

    functions with args

---
+++ function_with_args.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#how to create the function
+ welcomeNote () {
+    echo "------------------"
+    echo "welcome $1"
+    echo "age is $2"
+    echo "city is $3"
+    echo "------------------"
+}
+#to call the function 
+welcomeNote nag 28 nellore
+welcomeNote sruthi 20 nellore
+welcomeNote jaya 30 iskapalli
