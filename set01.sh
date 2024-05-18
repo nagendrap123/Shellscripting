62b1d68eff5a299e057dc417a5ad6e1e066c61b1
nagendrap123 <nagendrap0820@gmail.com>
2024-05-18 10:35:35 +0530

    set_command

--- set01.sh
+++ set01.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-# Invoke this script with three command line parameters,
-set $(uname -a) # Sets the positional parameters to the output
-# of the command `uname -a`
-echo "Positional parameters after set \`uname -a`\ :"
-# $1, $2, $3, etc. reinitialized to result of `uname -a`
+# Invoke this script with three command line parameters
+set $(uname -a)
+#positional parameters after set uname -a :
 echo "Field #1 of 'uname -a' = $1"
 echo "Field #2 of 'uname -a' = $2"
 echo "Field #3 of 'uname -a' = $3"
 echo "Field #3 of 'uname -a' = $@"
 exit 0
+
