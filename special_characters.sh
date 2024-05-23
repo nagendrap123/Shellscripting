206bcd2d8266b41bec2862726c5fb7bb9d64a5dd
nagendrap123 <nagendrap0820@gmail.com>
2024-05-23 11:41:51 +0530

    spl_chhars

---
+++ special_characters.sh
@@ -0,0 +1,16 @@
+In Linux, certain escaped characters have special meanings when used in commands or within double-quoted strings.
+Here are some of the most commonly used special escaped characters:
+
+1. **`\n`**: Represents a newline character. When encountered in a string, it creates a line break.
+
+2. **`\t`**: Represents a tab character. When encountered in a string, it creates a horizontal tab space.
+
+3. **`\r`**: Represents a carriage return character. It moves the cursor to the beginning of the line without advancing to the next line.
+
+4. **`\b`**: Represents a backspace character. It moves the cursor one character backward, effectively erasing the character before it.
+
+5. **`\f`**: Represents a form feed character. It causes the output device to advance to the next page or clear the screen, depending on the context.
+
+6. **`\v`**: Represents a vertical tab character. It causes the output device to move to the next vertical tab position.
+
+7. **`\\`**: Represents a backslash character itself. This is used to escape a backslash when you want to include it as part of a string.
