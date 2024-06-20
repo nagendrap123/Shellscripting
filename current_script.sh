aeb92610052185db9c4a9672041af9bb5d1e6caf
nagendrap123 <nagendrap0820@gmail.com>
2024-06-20 18:26:06 +0530

    msql

--- current_script.sh
+++ current_script.sh
@@ -2,6 +2,6 @@
 course="deveops from current script"
 echo "before calling the other script, course: $course"
 echo "process ID of current shell script: $$"
-./ other_script.sh
+./other_script.sh
 echo "aftre calling the other script, course: $course"
 echo "process ID of current shell script: $$"
